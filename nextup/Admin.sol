fb598032f46afec42062426349707e97c1941139
M-hassan-ch <hassanch5209@gmail.com>
2023-01-06 19:16:41 +0500

    added a modifier

--- nextup/Admin.sol
+++ nextup/Admin.sol
@@ -24,20 +24,20 @@ contract Admin is ERC20, Ownable{
         address contractAddress;
         bool isDisabled;
         uint maxSupply;
-        uint currentSupply;
+        uint suppliedAmount;
     }
 
     uint _athleteId;
 
 //  mapping(Athelete => Athlete ERC20 token details)
     mapping (uint => AthleteERC20Details) public _athleteERC20Detail;
-
+//  mapping(Athelete => Athlete token drops)
     mapping(uint => TokenDrop[]) public _athleteTokenDrops;
     
     AthleteERC20 _athleteERC20Contract;
     Admin _self;
 
-    constructor(uint maxSupply, uint priceInwei) ERC20("NextUp", "NXT") {
+    constructor(string memory nxtTokenName, string memory nxtTokenSymbol, uint maxSupply, uint priceInwei) ERC20(nxtTokenName, nxtTokenSymbol) {
         require(maxSupply > 0, "Admin: Max supply should be greater than zero");
         require(priceInwei > 0, "Admin: Price of token should be greater than zero");
         
@@ -51,6 +51,8 @@ contract Admin is ERC20, Ownable{
         // approve(address(this), initialMint);
     }
 
+    // -------------------------- NextUp token related functions --------------------------
+
     function increaseNxtTokenMaxSupply(uint updatedSupply) public onlyOwner{
         require(updatedSupply > 0, "Admin: Updated supply should be greater than zero");
         _nxtTokenMaxSupply += updatedSupply;
@@ -59,8 +61,6 @@ contract Admin is ERC20, Ownable{
     function updateNxtTokenPrice(uint updatedPrice) public onlyOwner{
         _pricePerNxtToken = updatedPrice;
     }
-
-    // -------------------- View Function ------------------------
 
     function getNxtTokenMaxSupply() public view onlyOwner returns(uint){
         return _nxtTokenMaxSupply;
@@ -81,7 +81,7 @@ contract Admin is ERC20, Ownable{
 // -------------------------- User related functions --------------------------
 
 //  In case, if customer is buying NextUp tokens in WEI
-    function buyTokenInWei(uint amountToBuy) public payable
+    function buyNxtTokenInWei(uint amountToBuy) public payable
     {
         require(msg.sender != address(0), "Admin: Caller is null address");
         require(msg.value == (_pricePerNxtToken * amountToBuy), "UtilityToken: Insufficient amount to buy tokens");
@@ -103,16 +103,21 @@ contract Admin is ERC20, Ownable{
 
 // -------------------------- Athlete related functions --------------------------
 
-//   Delete an athlete profile is missing
+//  Delete an athlete profile is missing
 
-    modifier validAthlete(uint athleteId){
-        require(_athleteERC20Detail[athleteId].contractAddress != address(0), "Admin: Athlete account donot exists");
+    modifier isValidAthlete(uint athleteId){
+        require(_athleteERC20Detail[athleteId].contractAddress != address(0), "Admin: Athlete account not found");
+        _;
+    }
+
+    modifier isAthleteNotDisabled(uint athleteId){
+        require(_athleteERC20Detail[athleteId].isDisabled == false, "Admin: Athlete account is disabled");
         _;
     }
 
 //  Before calling this function admin has already deployed AthleteERC20 contract
 //  ERC20 token name and symbol already had given during the deployement
-//  Creating an athlete with drops. This function returns id of created athlete
+//  Creating an athlete with drops(sorted array). This function returns id of created athlete
     function createAthlete(AthleteERC20Details memory athleteDetails, TokenDrop[] memory tokenDrops)  public onlyOwner returns(uint){
         require(athleteDetails.contractAddress != address(0), "Admin: Athlete ERC20 contract cant be null");
         require(athleteDetails.maxSupply > 0, "Admin: Total supply should be greater than zero");
@@ -129,42 +134,14 @@ contract Admin is ERC20, Ownable{
         return _athleteId-1;
     }
 
-//  Admin call this function to update the price(In NXT tokens) of Athlete's ERC20 Token
-    function updateAthleteTokenPrice(uint athleteId, uint price) public onlyOwner validAthlete(athleteId){
-        _athleteERC20Detail[athleteId].pricePerToken = price;
-    }
-
-//  Admin call this function to increase the max supply of Athlete's ERC20 Token
-    function updateAthleteTokenMaxSupply(uint athleteId, uint addSupply) public onlyOwner validAthlete(athleteId){
-        require(addSupply > 1, "Admin: Amount of supply you wanted to increase should be greater than zero");
-        _athleteERC20Detail[athleteId].maxSupply += addSupply;
-    }
-
-//  Admin call this function to increase the current supply of Athlete's ERC20 Token
-    function updateAthleteTokenCurrentSupply(uint athleteId, uint newSupply) public onlyOwner validAthlete(athleteId){
-        require(newSupply > 0, "Admin: Supply should be greater than zero");
-        require(_athleteERC20Detail[athleteId].currentSupply + newSupply <= _athleteERC20Detail[athleteId].maxSupply, "Admin: Cant mint tokens greater than max supply");
-        
-        
-        _athleteERC20Detail[athleteId].currentSupply += newSupply;
-    }
-    
-//  Admin call this function to enable Athlete profile
-    function enableAthleteProfile(uint athleteId) public onlyOwner validAthlete(athleteId){
-        _athleteERC20Detail[athleteId].isDisabled = false;
-    }
-
-//  Admin call this function to disable Athlete profile
-    function disableAthleteProfile(uint athleteId) public onlyOwner validAthlete(athleteId){
-        _athleteERC20Detail[athleteId].isDisabled = true;
-    }
-
-//  Internal function to check whether invalid data exists in the token drops.
+    //  Internal function to check whether invalid data exists in the token drops.
     function validateDropDates(TokenDrop[] memory drops) view internal onlyOwner returns(bool){
+
+        uint currentTime = block.timestamp;
 
         for (uint i=0; i<drops.length; i++)
         {
-            if ((drops[i].timestamp < block.timestamp) || drops[i].price<=0 || drops[i].supply<=0){
+            if ((drops[i].timestamp < currentTime) || drops[i].price<=0 || drops[i].supply<=0 || drops[i].isApplied==true){
                 return false;
             }
         }
@@ -172,12 +149,34 @@ contract Admin is ERC20, Ownable{
         return true;
     }
 
-    function getValidTokenDrop(uint athleteId) public onlyOwner validAthlete(athleteId) returns(TokenDrop memory){
+//  Admin call this function to update the price(In NXT tokens) of Athlete's ERC20 Token
+    function updateAthleteTokenPrice(uint athleteId, uint price) public onlyOwner isValidAthlete(athleteId){
+        _athleteERC20Detail[athleteId].pricePerToken = price;
+    }
+
+//  Admin call this function to increase the max supply of Athlete's ERC20 Token
+    function increaseAthleteTokenMaxSupply(uint athleteId, uint addSupply) public onlyOwner isValidAthlete(athleteId){
+        require(addSupply > 0, "Admin: Amount of supply you wanted to increase should be greater than zero");
+        _athleteERC20Detail[athleteId].maxSupply += addSupply;
+    }
+
+    
+//  Admin call this function to enable Athlete profile
+    function enableAthleteProfile(uint athleteId) public onlyOwner isValidAthlete(athleteId){
+        _athleteERC20Detail[athleteId].isDisabled = false;
+    }
+
+//  Admin call this function to disable Athlete profile
+    function disableAthleteProfile(uint athleteId) public onlyOwner isValidAthlete(athleteId){
+        _athleteERC20Detail[athleteId].isDisabled = true;
+    }
+
+    function getValidTokenDrop(uint athleteId) public view onlyOwner isValidAthlete(athleteId) returns(TokenDrop memory){
         for (uint i=0; i<_athleteTokenDrops[athleteId].length;i++){    
             
             if (_athleteTokenDrops[athleteId][i].timestamp >= block.timestamp && !_athleteTokenDrops[athleteId][i].isApplied){
                 
-                _athleteTokenDrops[athleteId][i].isApplied = true;
+                // _athleteTokenDrops[athleteId][i].isApplied = true;
                 
                 return _athleteTokenDrops[athleteId][i];
             }
@@ -189,7 +188,7 @@ contract Admin is ERC20, Ownable{
     }
 
 //  Get all the token drops asociated with an athlete
-    function getTokenDrops(uint athleteId) public view validAthlete(athleteId) returns(TokenDrop[] memory){
+    function getTokenDrops(uint athleteId) public view isValidAthlete(athleteId) returns(TokenDrop[] memory){
         return _athleteTokenDrops[athleteId];
     }
 
